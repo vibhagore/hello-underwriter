cf783d482f07421225900faf9094c968f0393479
vibhagore <150468105+vibhagore@users.noreply.github.com>
2023-11-10 16:28:57 -0600

    Update app.py

--- pages/app.py
+++ pages/app.py
@@ -4,7 +4,7 @@ from streamlit.hello.utils import show_code
 
 # Sidebar contents
 with st.sidebar:
-    st.title('🤗💬 LLM Chat App')
+    st.title('🤗💬 LLM Chat with the Virtual Underwriter App')
     st.markdown('''
     ## About
     This app is an LLM-powered chatbot built using:
@@ -18,59 +18,12 @@ with st.sidebar:
  
  
 def underwriter():
-    st.header("Chat with PDF 💬")
+    st.write("Hello")
+    st.header("Chat with Underwriter - PDF 💬")
  
  
-    # upload a PDF file
-    pdf = st.file_uploader("Upload your PDF", type='pdf')
- 
-    # st.write(pdf)
-    if pdf is not None:
-        pdf_reader = PdfReader(pdf)
-        
-        text = ""
-        for page in pdf_reader.pages:
-            text += page.extract_text()
- 
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000,
-            chunk_overlap=200,
-            length_function=len
-            )
-        chunks = text_splitter.split_text(text=text)
- 
-        # # embeddings
-        store_name = pdf.name[:-4]
-        st.write(f'{store_name}')
-        # st.write(chunks)
- 
-        if os.path.exists(f"{store_name}.pkl"):
-            with open(f"{store_name}.pkl", "rb") as f:
-                VectorStore = pickle.load(f)
-            # st.write('Embeddings Loaded from the Disk')s
-        else:
-            embeddings = OpenAIEmbeddings()
-            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-            with open(f"{store_name}.pkl", "wb") as f:
-                pickle.dump(VectorStore, f)
- 
-        # embeddings = OpenAIEmbeddings()
-        # VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
- 
-        # Accept user questions/query
-        query = st.text_input("Ask questions about your PDF file:")
-        # st.write(query)
- 
-        if query:
-            docs = VectorStore.similarity_search(query=query, k=3)
- 
-            llm = OpenAI()
-            chain = load_qa_chain(llm=llm, chain_type="stuff")
-            with get_openai_callback() as cb:
-                response = chain.run(input_documents=docs, question=query)
-                print(cb)
-            st.write(response)
+    
  
 #if __name__ == '__underwriter__':
- #   underwriter()
+    underwriter()
   
