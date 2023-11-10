2921069929933bdd32d93b79868950d7e76d301f
vibhagore <150468105+vibhagore@users.noreply.github.com>
2023-11-10 17:10:18 -0600

    Update app.py

--- pages/app.py
+++ pages/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit.hello.utils import show_code
 from PyPDF2 import PdfReader
-
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 # Sidebar contents
 with st.sidebar:
@@ -30,7 +30,14 @@ def underwriter() -> None:
         for page in pdf_reader.pages:
             text += page.extract_text()
 
-        st.write(text)
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+            )
+        chunks = text_splitter.split_text(text=text)
+
+        st.write(chunks)
       
 underwriter()
 
