d10303a0ef0aba0d46e5103ac3bdf810f8fabf40
vibhagore <150468105+vibhagore@users.noreply.github.com>
2023-11-15 13:38:22 -0600

    Update app.py

--- pages/app.py
+++ pages/app.py
@@ -25,22 +25,21 @@ with st.sidebar:
     #add_vertical_space(5)
     st.write('Made with ❤️ by [Prompt Engineer](https://youtube.com/@engineerprompt)')
  
- 
-def underwriter() -> None:
-    st.header("Get the API Key")
-    openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
-
 def generate_response(input_text):
     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
     st.info(llm(input_text))
 
-with st.form('my_form'):
-    text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
-    submitted = st.form_submit_button('Submit')
-    if not openai_api_key.startswith('sk-'):
-        st.warning('Please enter your OpenAI API key!', icon='⚠')
-    if submitted and openai_api_key.startswith('sk-'):
-        generate_response(text)
+def underwriter() -> None:
+    st.header("Get the API Key")
+    openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+
+   with st.form('my_form'):
+       text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+       submitted = st.form_submit_button('Submit')
+       if not openai_api_key.startswith('sk-'):
+           st.warning('Please enter your OpenAI API key!', icon='⚠')
+       if submitted and openai_api_key.startswith('sk-'):
+           generate_response(text)
     
     st.header("Chat with Virtual Underwriter")
     # upload a PDF file
@@ -75,6 +74,6 @@ with st.form('my_form'):
 
         st.write(chunks)
       
-underwriter()
+#underwriter()
 
 #show_code(underwriter)
