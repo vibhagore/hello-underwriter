4be9ccdeac87463e7e079efdc8e1078bf4731ad6
vibhagore <150468105+vibhagore@users.noreply.github.com>
2023-11-15 13:50:25 -0600

    Update app.py

--- pages/app.py
+++ pages/app.py
@@ -15,7 +15,7 @@ def generate_response(input_text):
     st.info(llm(input_text))
 
 with st.form('my_form'):
-       text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+       text = st.text_area('Enter text:', 'zenter the open API key here')
        submitted = st.form_submit_button('Submit')
        if not openai_api_key.startswith('sk-'):
            st.warning('Please enter your OpenAI API key!', icon='⚠')
