0c21f0751a0649bef1313e7f7533558472f8b306
vibhagore <150468105+vibhagore@users.noreply.github.com>
2023-11-15 13:41:27 -0600

    Update app.py

--- pages/app.py
+++ pages/app.py
@@ -28,14 +28,14 @@ with st.sidebar:
     st.header("Get the API Key")
     openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
- with st.form('my_form'):
-    text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
-    submitted = st.form_submit_button('Submit')
-    if not openai_api_key.startswith('sk-'):
-        st.warning('Please enter your OpenAI API key!', icon='⚠')
-    if submitted and openai_api_key.startswith('sk-'):
-        generate_response(text)
-        underwriter()
+    with st.form('my_form'):
+       text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+       submitted = st.form_submit_button('Submit')
+       if not openai_api_key.startswith('sk-'):
+           st.warning('Please enter your OpenAI API key!', icon='⚠')
+       if submitted and openai_api_key.startswith('sk-'):
+           generate_response(text)
+           underwriter()
  
 def generate_response(input_text):
     llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
